28f785a07b41274ed0c86b181acb65ab7981f1eb
PatrickMontes <alexandermontes30p@hotmail.com>
2024-04-16 18:19:18 -0500

    Estructura del Proyecto

---
+++ src/main/java/pe/edu/cibertec/proyectodaw1/configuration/AppConfig.java
@@ -0,0 +1,4 @@
+package pe.edu.cibertec.proyectodaw1.configuration;
+
+public class AppConfig {
+}
---
+++ src/test/java/pe/edu/cibertec/proyectodaw1/ProyectoDaw1ApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.proyectodaw1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoDaw1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
